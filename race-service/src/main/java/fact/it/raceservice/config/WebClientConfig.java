e41d4d7334fc752c9977720075685068e4454b3d
Finn De Peuter <r0831240@student.thomasmore.be>
2023-11-22 10:44:01 +0100

    integration met frontend

--- race-service/src/main/java/fact/it/raceservice/config/WebClientConfig.java
+++ race-service/src/main/java/fact/it/raceservice/config/WebClientConfig.java
@@ -2,7 +2,15 @@ package fact.it.raceservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebClientConfig {
